087de4260e86bbd5046eaf8156001588457b0c5b
Anomaex <30316200+Anomaex@users.noreply.github.com>
2023-12-15 13:43:10 +0200

    Add server time url

--- src/lib/store.js
+++ src/lib/store.js
@@ -3,12 +3,14 @@ export const URLs = {
     EXCHANGE_INFO: "/fapi/v1/exchangeInfo",
     LEVERAGE_BRACKET: "/fapi/v1/leverageBracket",
     LEVERAGE: "/fapi/v1/leverage",
-    MARGIN_TYPE: "/fapi/v1/marginType"
+    MARGIN_TYPE: "/fapi/v1/marginType",
+    SERVER_TIME: "/fapi/v1/time"
 };
 
 export const userData = {
     key: "",
-    secret: ""
+    secret: "",
+    recvWindow: 0
 };
 
 export const symbols = [{ symbol: "BTCUSDT", maxLeverage: 125 }]; // { symbol: "BTCUSDT", maxLeverage: 125 }
